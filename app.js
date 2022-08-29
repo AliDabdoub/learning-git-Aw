0aa69474b02be44ff3b02db7851e44b14ee4d91b
Ali Dabdoub <ali.dabdoub@outlook.com>
2022-08-29 14:50:12 +0200

    many console.log

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('app.js')
+console.log('app.js')
+console.log('app.js')
+console.log('app.js')
+console.log('app.js')
+console.log('app.js')
+console.log('app.js')
+console.log('app.js')
